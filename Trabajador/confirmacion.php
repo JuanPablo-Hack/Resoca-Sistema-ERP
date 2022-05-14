c07d063934c252598845ac2fad6060b78cad8581
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesus_figueroa@ucol.mx>
2022-05-14 11:37:35 -0500

    Se pudo realizar el cambio de la firma digital Gracias a este cambio ya vamos a poder cambiar muchas cosas ahorita subiremos los cambios para que se visualizen

--- Trabajador/confirmacion.php
+++ Trabajador/confirmacion.php
@@ -186,8 +186,177 @@ if ($Row = mysqli_fetch_array($result)) {
   <script type="text/javascript" src="../assets/lib/bootstrap-daterangepicker/moment.min.js"></script>
   <script type="text/javascript" src="../assets/lib/bootstrap-timepicker/js/bootstrap-timepicker.js"></script>
   <script src="../assets/lib/advanced-form-components.js"></script>
-  <script src="./js/script.js"></script>
   <script src="../assets/lib/sweetalert2/sweetalert2.all.min.js"></script>
+  <script>
+    //======================================================================
+    // VARIABLES
+    //======================================================================
+    let miCanvas = document.querySelector("#canvas");
+    let lineas = [];
+    let correccionX = 0;
+    let correccionY = 0;
+    let pintarLinea = false;
+    // Marca el nuevo punto
+    let nuevaPosicionX = 0;
+    let nuevaPosicionY = 0;
+
+    let posicion = miCanvas.getBoundingClientRect();
+    correccionX = posicion.x;
+    correccionY = posicion.y;
+
+    miCanvas.width = 250;
+    miCanvas.height = 150;
+
+    //======================================================================
+    // FUNCIONES
+    //======================================================================
+
+    /**
+     * Funcion que empieza a dibujar la linea
+     */
+    function empezarDibujo() {
+      pintarLinea = true;
+      lineas.push([]);
+    }
+
+    /**
+     * Funcion que guarda la posicion de la nueva línea
+     */
+    function guardarLinea() {
+      lineas[lineas.length - 1].push({
+        x: nuevaPosicionX,
+        y: nuevaPosicionY,
+      });
+    }
+
+    /**
+     * Funcion dibuja la linea
+     */
+    function dibujarLinea(event) {
+      event.preventDefault();
+      if (pintarLinea) {
+        let ctx = miCanvas.getContext("2d");
+        // Estilos de linea
+        ctx.lineJoin = ctx.lineCap = "round";
+        ctx.lineWidth = 2;
+        // Color de la linea
+        ctx.strokeStyle = "#000000";
+        // Marca el nuevo punto
+        if (event.changedTouches == undefined) {
+          // Versión ratón
+          nuevaPosicionX = event.layerX;
+          nuevaPosicionY = event.layerY;
+        } else {
+          // Versión touch, pantalla tactil
+          nuevaPosicionX = event.changedTouches[0].pageX - correccionX;
+          nuevaPosicionY = event.changedTouches[0].pageY - correccionY;
+        }
+        // Guarda la linea
+        guardarLinea();
+        // Redibuja todas las lineas guardadas
+        ctx.beginPath();
+        lineas.forEach(function(segmento) {
+          ctx.moveTo(segmento[0].x, segmento[0].y);
+          segmento.forEach(function(punto, index) {
+            ctx.lineTo(punto.x, punto.y);
+          });
+        });
+        ctx.stroke();
+      }
+    }
+
+    /**
+     * Funcion que deja de dibujar la linea
+     */
+    function pararDibujar() {
+      pintarLinea = false;
+      guardarLinea();
+    }
+
+    //======================================================================
+    // EVENTOS
+    //======================================================================
+
+    // Eventos raton
+    miCanvas.addEventListener("mousedown", empezarDibujo, false);
+    miCanvas.addEventListener("mousemove", dibujarLinea, false);
+    miCanvas.addEventListener("mouseup", pararDibujar, false);
+
+    // Eventos pantallas táctiles
+    miCanvas.addEventListener("touchstart", empezarDibujo, false);
+    miCanvas.addEventListener("touchmove", dibujarLinea, false);
+    document.addEventListener("DOMContentLoaded", function() {
+      document
+        .getElementById("formConfirmar")
+        .addEventListener("submit", confirmar);
+    });
+    async function confirmar(e) {
+      e.preventDefault();
+      const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: "btn btn-success",
+          cancelButton: "btn btn-danger",
+        },
+        buttonsStyling: false,
+      });
+
+      swalWithBootstrapButtons
+        .fire({
+          title: "Estas seguro que la información es la correcta?",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Si, agregar confirmación",
+          cancelButtonText: "No, cancelar!",
+          reverseButtons: true,
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            var form = document.getElementById("formConfirmar");
+            const image = miCanvas.toDataURL("image/png");
+            const fd = new FormData(form);
+            fd.append("imagen", image);
+            fd.append("accion", "agregar");
+            fetch("php/confirmacion_controller.php", {
+                method: "POST",
+                body: fd,
+              })
+              .then((result) => result.text())
+              .then((result) => {
+                if (result == 1) {
+                  swalWithBootstrapButtons.fire(
+                    "Agregado!",
+                    "La dirección ip ha sido agregado en la base de datos.",
+                    "success"
+                  );
+                  form.reset();
+                  setTimeout(function() {
+                    location.reload();
+                  }, 2000);
+                } else {
+                  swalWithBootstrapButtons.fire(
+                    "Error",
+                    "Hemos tenido un error a la base de datos o la conexión.",
+                    "error"
+                  );
+                  // form.reset();
+                  // setTimeout(function() {
+                  //   location.reload();
+                  // }, 2000);
+                }
+              });
+          } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+          ) {
+            swalWithBootstrapButtons.fire(
+              "Cancelado",
+              "Revise su información de nuevo",
+              "error"
+            );
+          }
+        });
+    }
+  </script>
 </body>
 
 </html>
--- Trabajador/js/script.js
+++ Trabajador/js/script.js
@@ -1,152 +1,192 @@
-const $canvas = document.querySelector("#canvas"),
-    $btnLimpiar = document.querySelector("#btnLimpiar"),
-    $btnEnviar = document.querySelector("#btnEnviar");
-const contexto = $canvas.getContext("2d");
-const COLOR_PINCEL = "black";
-const COLOR_FONDO = "white";
-const GROSOR = 2;
-let xAnterior = 0,
-    yAnterior = 0,
-    xActual = 0,
-    yActual = 0;
-const obtenerXReal = (clientX) =>
-    clientX - $canvas.getBoundingClientRect().left;
-const obtenerYReal = (clientY) => clientY - $canvas.getBoundingClientRect().top;
-let haComenzadoDibujo = false; // Bandera que indica si el usuario está presionando el botón del mouse sin soltarlo
+//======================================================================
+// VARIABLES
+//======================================================================
+let miCanvas = document.querySelector("#canvas");
+let lineas = [];
+let correccionX = 0;
+let correccionY = 0;
+let pintarLinea = false;
+// Marca el nuevo punto
+let nuevaPosicionX = 0;
+let nuevaPosicionY = 0;
 
-const limpiarCanvas = () => {
-    // Colocar color blanco en fondo de canvas
-    contexto.fillStyle = COLOR_FONDO;
-    contexto.fillRect(0, 0, $canvas.width, $canvas.height);
-};
-limpiarCanvas();
-$btnLimpiar.onclick = limpiarCanvas;
+let posicion = miCanvas.getBoundingClientRect();
+correccionX = posicion.x;
+correccionY = posicion.y;
 
-// Lo demás tiene que ver con pintar sobre el canvas en los eventos del mouse
-$canvas.addEventListener("mousedown", (evento) => {
-    // En este evento solo se ha iniciado el clic, así que dibujamos un punto
-    xAnterior = xActual;
-    yAnterior = yActual;
-    xActual = obtenerXReal(evento.clientX);
-    yActual = obtenerYReal(evento.clientY);
-    contexto.beginPath();
-    contexto.fillStyle = COLOR_PINCEL;
-    contexto.fillRect(xActual, yActual, GROSOR, GROSOR);
-    contexto.closePath();
-    // Y establecemos la bandera
-    haComenzadoDibujo = true;
-});
+miCanvas.width = 250;
+miCanvas.height = 150;
 
-$canvas.addEventListener("mousemove", (evento) => {
-    if (!haComenzadoDibujo) {
-        return;
+//======================================================================
+// FUNCIONES
+//======================================================================
+
+/**
+ * Funcion que empieza a dibujar la linea
+ */
+function empezarDibujo() {
+  pintarLinea = true;
+  lineas.push([]);
+}
+
+/**
+ * Funcion que guarda la posicion de la nueva línea
+ */
+function guardarLinea() {
+  lineas[lineas.length - 1].push({
+    x: nuevaPosicionX,
+    y: nuevaPosicionY,
+  });
+}
+
+/**
+ * Funcion dibuja la linea
+ */
+function dibujarLinea(event) {
+  event.preventDefault();
+  if (pintarLinea) {
+    let ctx = miCanvas.getContext("2d");
+    // Estilos de linea
+    ctx.lineJoin = ctx.lineCap = "round";
+    ctx.lineWidth = 2;
+    // Color de la linea
+    ctx.strokeStyle = "#000000";
+    // Marca el nuevo punto
+    if (event.changedTouches == undefined) {
+      // Versión ratón
+      nuevaPosicionX = event.layerX;
+      nuevaPosicionY = event.layerY;
+    } else {
+      // Versión touch, pantalla tactil
+      nuevaPosicionX = event.changedTouches[0].pageX - correccionX;
+      nuevaPosicionY = event.changedTouches[0].pageY - correccionY;
     }
-    // El mouse se está moviendo y el usuario está presionando el botón, así que dibujamos todo
-
-    xAnterior = xActual;
-    yAnterior = yActual;
-    xActual = obtenerXReal(evento.clientX);
-    yActual = obtenerYReal(evento.clientY);
-    contexto.beginPath();
-    contexto.moveTo(xAnterior, yAnterior);
-    contexto.lineTo(xActual, yActual);
-    contexto.strokeStyle = COLOR_PINCEL;
-    contexto.lineWidth = GROSOR;
-    contexto.stroke();
-    contexto.closePath();
-});
-["mouseup", "mouseout"].forEach((nombreDeEvento) => {
-    $canvas.addEventListener(nombreDeEvento, () => {
-        haComenzadoDibujo = false;
+    // Guarda la linea
+    guardarLinea();
+    // Redibuja todas las lineas guardadas
+    ctx.beginPath();
+    lineas.forEach(function (segmento) {
+      ctx.moveTo(segmento[0].x, segmento[0].y);
+      segmento.forEach(function (punto, index) {
+        ctx.lineTo(punto.x, punto.y);
+      });
     });
-});
+    ctx.stroke();
+  }
+}
 
-// $btnEnviar.onclick = async () => {
-//   // Convertir la imagen a Base64 y ponerlo en el enlace
-//   var form = document.getElementById("formConfirmar");
-//   const image = $canvas.toDataURL("image/png");
-//   const fd = new FormData(form);
-//   fd.append("imagen", image); // Se llama "imagen", en PHP lo recuperamos con $_POST["imagen"]
-//   const respuestaHttp = await fetch("php/confirmacion.php", {
-//     method: "POST",
-//     body: fd,
-//   });
-//   const nombreImagenSubida = await respuestaHttp.json();
-//   console.log(
-//     "La imagen ha sido enviada y tiene el nombre de: " + nombreImagenSubida
-//   );
-// };
+/**
+ * Funcion que deja de dibujar la linea
+ */
+function pararDibujar() {
+  pintarLinea = false;
+  guardarLinea();
+}
 
-document.addEventListener("DOMContentLoaded", function() {
-    document
-        .getElementById("formConfirmar")
-        .addEventListener("submit", confirmar);
+//======================================================================
+// EVENTOS
+//======================================================================
+
+// Eventos raton
+miCanvas.addEventListener("mousedown", empezarDibujo, false);
+miCanvas.addEventListener("mousemove", dibujarLinea, false);
+miCanvas.addEventListener("mouseup", pararDibujar, false);
+
+// Eventos pantallas táctiles
+miCanvas.addEventListener("touchstart", empezarDibujo, false);
+miCanvas.addEventListener("touchmove", dibujarLinea, false);
+
+
+// // $btnEnviar.onclick = async () => {
+// //   // Convertir la imagen a Base64 y ponerlo en el enlace
+// //   var form = document.getElementById("formConfirmar");
+// //   const image = $canvas.toDataURL("image/png");
+// //   const fd = new FormData(form);
+// //   fd.append("imagen", image); // Se llama "imagen", en PHP lo recuperamos con $_POST["imagen"]
+// //   const respuestaHttp = await fetch("php/confirmacion.php", {
+// //     method: "POST",
+// //     body: fd,
+// //   });
+// //   const nombreImagenSubida = await respuestaHttp.json();
+// //   console.log(
+// //     "La imagen ha sido enviada y tiene el nombre de: " + nombreImagenSubida
+// //   );
+// // };
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("formConfirmar")
+    .addEventListener("submit", confirmar);
 });
 
 async function confirmar(e) {
-    e.preventDefault();
-    var form = document.getElementById("ip_Form");
-    const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-            confirmButton: "btn btn-success",
-            cancelButton: "btn btn-danger",
-        },
-        buttonsStyling: false,
-    });
+  e.preventDefault();
+  console.log("Convertir");
+//   const image = $canvas.toDataURL("image/png");
+//   var form = document.getElementById("formConfirmar");
 
-    swalWithBootstrapButtons
-        .fire({
-            title: "Estas seguro que la información es la correcta?",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonText: "Si, agregar confirmación",
-            cancelButtonText: "No, cancelar!",
-            reverseButtons: true,
-        })
-        .then((result) => {
-            if (result.isConfirmed) {
-                var form = document.getElementById("formConfirmar");
-                const image = $canvas.toDataURL("image/png");
-                const fd = new FormData(form);
-                fd.append("imagen", image);
-                fd.append("accion", "agregar");
-                fetch("php/confirmacion_controller.php", {
-                        method: "POST",
-                        body: fd,
-                    })
-                    .then((result) => result.text())
-                    .then((result) => {
-                        if (result == 1) {
-                            swalWithBootstrapButtons.fire(
-                                "Agregado!",
-                                "La dirección ip ha sido agregado en la base de datos.",
-                                "success"
-                            );
-                            form.reset();
-                            setTimeout(function() {
-                                location.reload();
-                            }, 2000);
-                        } else {
-                            swalWithBootstrapButtons.fire(
-                                "Error",
-                                "Hemos tenido un error a la base de datos o la conexión.",
-                                "error"
-                            );
-                            form.reset();
-                            setTimeout(function() {
-                                location.reload();
-                            }, 2000);
-                        }
-                    });
-            } else if (
-                /* Read more about handling dismissals below */
-                result.dismiss === Swal.DismissReason.cancel
-            ) {
-                swalWithBootstrapButtons.fire(
-                    "Cancelado",
-                    "Revise su información de nuevo",
-                    "error"
-                );
-            }
-        });
+//   const swalWithBootstrapButtons = Swal.mixin({
+//     customClass: {
+//       confirmButton: "btn btn-success",
+//       cancelButton: "btn btn-danger",
+//     },
+//     buttonsStyling: false,
+//   });
+
+//   swalWithBootstrapButtons
+//     .fire({
+//       title: "Estas seguro que la información es la correcta?",
+//       icon: "warning",
+//       showCancelButton: true,
+//       confirmButtonText: "Si, agregar confirmación",
+//       cancelButtonText: "No, cancelar!",
+//       reverseButtons: true,
+//     })
+//     .then((result) => {
+//       if (result.isConfirmed) {
+//         var form = document.getElementById("formConfirmar");
+//         const image = $canvas.toDataURL("image/png");
+//         const fd = new FormData(form);
+//         fd.append("imagen", image);
+//         fd.append("accion", "agregar");
+//         fetch("php/confirmacion_controller.php", {
+//           method: "POST",
+//           body: fd,
+//         })
+//           .then((result) => result.text())
+//           .then((result) => {
+//             if (result == 1) {
+//               swalWithBootstrapButtons.fire(
+//                 "Agregado!",
+//                 "La dirección ip ha sido agregado en la base de datos.",
+//                 "success"
+//               );
+//               form.reset();
+//               setTimeout(function () {
+//                 location.reload();
+//               }, 2000);
+//             } else {
+//               swalWithBootstrapButtons.fire(
+//                 "Error",
+//                 "Hemos tenido un error a la base de datos o la conexión.",
+//                 "error"
+//               );
+//               form.reset();
+//               setTimeout(function () {
+//                 location.reload();
+//               }, 2000);
+//             }
+//           });
+//       } else if (
+//         /* Read more about handling dismissals below */
+//         result.dismiss === Swal.DismissReason.cancel
+//       ) {
+//         swalWithBootstrapButtons.fire(
+//           "Cancelado",
+//           "Revise su información de nuevo",
+//           "error"
+//         );
+//       }
+//     });
 }
+
