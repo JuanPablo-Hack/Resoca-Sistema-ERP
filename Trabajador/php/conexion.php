f4b1c96b249b8b308d2167c3a97e5b8a7a78399b
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesus_figueroa@ucol.mx>
2022-05-16 16:28:52 -0500

    Haciendo la conexion

--- Trabajador/php/conexion.php
+++ Trabajador/php/conexion.php
@@ -1,14 +1,14 @@
 <?php
 
-$nombreServidor = "localhost";
-$nombreUsuario = "root";
-$passwordBaseDeDatos = "";
-$nombreBaseDeDatos = "resoca";
+// $nombreServidor = "localhost";
+// $nombreUsuario = "root";
+// $passwordBaseDeDatos = "";
+// $nombreBaseDeDatos = "resoca";
 
-// $nombreServidor = "213.190.6.1";
-// $nombreUsuario = "u298097659_resoca";
-// $passwordBaseDeDatos = "4so|8PVj>zR";
-// $nombreBaseDeDatos = "u298097659_resoca";
+$nombreServidor = "213.190.6.1";
+$nombreUsuario = "u298097659_resoca";
+$passwordBaseDeDatos = "4so|8PVj>zR";
+$nombreBaseDeDatos = "u298097659_resoca";
 
 
 
