161e6e57c4e494917e7d76229285f03dbdbc4661
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-03-23 13:06:25 -0600

    Cambios en el servide worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,10 +1,13 @@
 const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
   "/",
-  "/index.php",
-  "/css/style.css",
-  "/js/main.js",
-  "/js/app.js",
+  "/login.html",
+  "/assets/lib/font-awesome/css/font-awesome.css",
+  "/assets/css/style.cs",
+  "/assets/css/style-responsive.css",
+  "/assets/lib/jquery/jquery.min.js",
+  "/assets/lib/bootstrap/js/bootstrap.min.js",
+  "/assets/lib/jquery.backstretch.min.js",
 ];
 
 self.addEventListener("install", (installEvent) => {
